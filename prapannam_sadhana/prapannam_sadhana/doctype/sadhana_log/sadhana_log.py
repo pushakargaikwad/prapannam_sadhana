b51f872fbafb277fb94a5f7b1e899e12c0f25a06
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2024-10-04 12:20:58 +0000

    check if user is member before saving log

--- prapannam_sadhana/prapannam_sadhana/doctype/sadhana_log/sadhana_log.py
+++ prapannam_sadhana/prapannam_sadhana/doctype/sadhana_log/sadhana_log.py
@@ -9,6 +9,7 @@ class SadhanaLog(Document):
 	
 	def before_save(self):
 		self.validate_log_entry_doesnt_exist()
+		self.validate_user_is_member_of_group()
 
 	def validate_log_entry_doesnt_exist(self):
 		# check if entry already exists for given date for given user
@@ -19,3 +20,15 @@ class SadhanaLog(Document):
 		},)
 		if existing_entry:
 			frappe.throw("Entry already exists for given user for this date")
+
+	# check if user is member of the group 
+	# TODO: optimize this
+	def validate_user_is_member_of_group(self):
+		group = frappe.get_doc("Sadhana Group", self.group)
+
+		for member in group.members:
+			if member.user == self.by:
+				return
+			
+		frappe.throw(f"User {self.by} is not a member of {self.group}")
+		
