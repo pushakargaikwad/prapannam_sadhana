97362ff8075158a8d1c6d4e11f2077dfde1a9e56
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2024-10-04 11:33:44 +0000

    Dont allow duplicate entries for same day in Sadhanalog only one entry per user per day in a given group

--- prapannam_sadhana/prapannam_sadhana/doctype/sadhana_log/sadhana_log.py
+++ prapannam_sadhana/prapannam_sadhana/doctype/sadhana_log/sadhana_log.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2024, Pushakar Gaikwad and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SadhanaLog(Document):
-	pass
+	
+	def before_save(self):
+		self.validate_log_entry_doesnt_exist()
+
+	def validate_log_entry_doesnt_exist(self):
+		# check if entry already exists for given date for given user
+		existing_entry = frappe.db.exists("Sadhana Log", {
+			"by": self.by,
+			"date": self.date,
+			"group": self.group
+		},)
+		if existing_entry:
+			frappe.throw("Entry already exists for given user for this date")
