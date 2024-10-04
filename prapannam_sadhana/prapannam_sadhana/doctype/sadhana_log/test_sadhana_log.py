590278611398d6ddc3641feb993af8e8146c2f6d
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2024-10-04 10:18:03 +0000

    [ADD] Sadhana Log Doctype

---
+++ prapannam_sadhana/prapannam_sadhana/doctype/sadhana_log/test_sadhana_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Pushakar Gaikwad and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSadhanaLog(FrappeTestCase):
+	pass
